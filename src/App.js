8610c05d72759911097c721066cf43c7cb55fd22
taeone <taewon1554@gmail.com>
2022-10-14 15:49:07 +0900

    Edit App.js 윤태원

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Hello World??? 윤태원
+          Hello World?? 윤태원
       </header>
     </div>
   );
